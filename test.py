4f0876a45c6bbbee017f10ff053cf5d4a1cc4850
naittaleb <aittalebnabil@gmail.com>
2020-06-09 13:03:57 +0200

    Modifications induced by changing package name

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 from open3d import open3d
 import numpy as np
-import cgal_skel
+import romicgal
 
 mesh = open3d.io.read_triangle_mesh("sample/mesh.ply")
 vertices = np.asarray(mesh.vertices)
 faces = np.asarray(mesh.triangles)
 
-res = cgal_skel.skeletonize_mesh_with_corres(vertices, faces)
+res = romicgal.skeletonize_mesh_with_corres(vertices, faces)
