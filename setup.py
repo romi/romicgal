0b723c423babce4ba9d13b5c64f3e0e63e2a40b2
Jonathan Legrand <jonathan.legrand@ens-lyon.fr>
2024-01-22 23:31:53 +0100

    Improves `setup.py`.

--- setup.py
+++ setup.py
@@ -36,32 +36,57 @@ class get_pybind_include(object):
         import pybind11
         return pybind11.get_include(self.user)
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 with tempfile.TemporaryDirectory() as tmpdir:
     ext_modules = [
         Extension(
             'romicgal',
-            ['src/cgal_skel.cc'],
+            sources=['src/cgal_skel.cc'],
             include_dirs=[
                 # Path to pybind11 headers
                 get_pybind_include(),
                 get_pybind_include(user=True),
                 *get_include_dirs("eigen3"),
+                "/usr/include/", # required to include `CGAL/*`
+                "/usr/include/x86_64-linux-gnu/", # required to include `bits/timesize.h`
             ],
             language='c++',
             extra_compile_args=["-std=c++14"],
-            libraries=['gmp', 'mpfr']
+            libraries=['gmp', 'mpfr'],
         ),
     ]
-
     setup(
         name='romicgal',
         version='0.0.2',
-        ext_modules=ext_modules,
+        description='Python bindings, mostly to use `CGAL/extract_mean_curvature_flow_skeleton` with CGAL-5.4.1.',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
         author='Timothée Wintz',
         author_email='timothee@timwin.fr',
-        description='Python bindings, mostly to use `CGAL/extract_mean_curvature_flow_skeleton` with CGAL-5.4.1.',
-        install_requires=['pybind11>=2.4'],
-        long_description='',
+        maintainer='Jonathan Legrand',
+        maintainer_email='jonathan.legrand@ens-lyon.fr',
+        url='https://github.com/romi/romicgal',
+        download_url='',
+        ext_modules=ext_modules,
+        classifiers=[
+            "Programming Language :: C++",
+            "Programming Language :: Python :: 3",
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)"
+        ],
+        license="LGPL-3.0",
+        license_files='LICENSE.txt',
+        keywords=['ROMI', 'skeletonization', 'CGAL'],
+        platforms=['linux'],
         zip_safe=False,
+        install_requires=[
+            'pybind11 >=2.4',
+        ],
+        extras_require=[
+            'open3d >=0.9.0.0',
+        ],
+        python_requires='>=3.8',
     )
